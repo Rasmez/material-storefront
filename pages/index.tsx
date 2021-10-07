261addd333dd299af26216d4e1bbd4c11fdcb3aa
Ricardo Vasquez Rojas <ricardo.cdl.vr@gmail.com>
2021-10-07 08:15:26 -0400

    3rd attempt, deleting placeholders

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,36 +9,34 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import type { NextPage } from 'next';
 import * as React from 'react';
-import { LoginView } from '../components/auth/LoginView';
 import { ProductList } from '../components/ProductList';
 
 const Home: NextPage = () => {
   return (
     <React.Fragment>
-    <Box sx={{ flexGrow: 1, pb: 4 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Product List
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1, pb: 4 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Product List
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
       <CssBaseline />
       <Container fixed>
         <ProductList />
-        <LoginView />
       </Container>
     </React.Fragment>
   )
